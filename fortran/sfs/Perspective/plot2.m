fe734b4d23f317c0de9ddf182870792a97de967f
Sanath Keshav <saraswathiks@Sanaths-MacBook-Air.local>
2016-06-30 16:02:14 +0530

    removed fortran/sfs/Perspective/moz.txt

--- fortran/sfs/Perspective/plot2.m
+++ fortran/sfs/Perspective/plot2.m
@@ -1,6 +1,13 @@
 load op_neumannf.txt
 Z1 = op_neumannf(:,3);
-z2 = reshape(Z1,[201,201]);
-x = 0:1/200:1;
-y = 0:1/200:1;
-surf(x,y,z2,'EdgeColor','black')
+z2 = reshape(Z1,[200,200]);
+
+x = linspace(0,1,200);
+y = linspace(0,1,200);
+
+figure(2)
+surf(x,y,z2,'EdgeColor','none')
+colormap gray
+light('Position',[1 1 1],'Style','local')
+lighting gouraud
+axis([0.2,0.8,0.2,0.8,-1.8,-0.8])
