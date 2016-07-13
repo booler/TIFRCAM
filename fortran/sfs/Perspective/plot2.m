fed37679b64c9bcc59e236f356e2888a0ab6c96c
Sanath Keshav <sanathkeshav.mysore@gmail.com>
2016-07-13 17:42:41 +0530

    Minor

--- fortran/sfs/Perspective/plot2.m
+++ fortran/sfs/Perspective/plot2.m
@@ -6,7 +6,7 @@ x = linspace(0,1,300);
 y = linspace(0,1,300);
 
 figure(1)
-surf(x,y,gray(:,:,1)','EdgeColor','none')
+surf(x,y,z2','EdgeColor','none')
 
 %colormap gray
 colormap ([1 1 1]);
@@ -15,9 +15,9 @@ material ([0 1 0]);
 %set(gca,'visible','off')
 grid on;
 set(gca,'color','black')
-set(gcf,'color','black')
+%set(gcf,'color','black')
 light('Position',[0 0 100],'Style','local')
 lighting phong
 axis equal tight;
 
-%axis([0,1,0,1,1,1.4]);
+axis([0,1,0,1,-2,0]);
